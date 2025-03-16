73b7d74c8b6dc68d52e4344a8543c28bc5d051a1
OsamaDW <s12028609@stu.najah.edu>
2025-03-16 09:48:23 +0200

    design the function that add cards to the swiper

--- src/js/initailize-swipers.js
+++ src/js/initailize-swipers.js
@@ -1,8 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-    new Swiper(".swiper", {
-        loop: true,
-        pagination: { el: ".swiper-pagination", clickable: true },
-        navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
-    });
-
+const currentWidth = window.innerWidth <= 700 ? 1 : 4;
+const swiper = new Swiper(".swiper", {
+    slidesPerView: currentWidth,
+    slidesPerGroup: currentWidth,
+    loop: true,
+    pagination: { el: ".swiper-pagination", clickable: true },
+    navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
 });
+const updateSlidesPerView = () => {
+    swiper.params.slidesPerView = currentWidth;
+    swiper.params.slidesPerGroup= currentWidth;
+    swiper.update();
+};
+
+window.addEventListener("orientationchange", updateSlidesPerView);
